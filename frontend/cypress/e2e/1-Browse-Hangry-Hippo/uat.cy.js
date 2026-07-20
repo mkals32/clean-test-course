82b131faef71a304890ae45e4df64d03ed554b29
mkals32 <72354656+mkals32@users.noreply.github.com>
2026-07-20 23:50:30 +0200

    Check Cypress Test 6.1

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -4,8 +4,21 @@ describe('Shows Appeteasers', () => {
   });
   it('selects Handhelds', () => {
     //TODO: Add a test to check Handhelds
+    cy.contains('Handhelds').click();
+    //Check if Cheese Burger is there.
+    cy.contains('Cheese Burger');
+    //Check if Fajita Tacos is there
+    cy.contains('Fajita Tacos');
   });
   it('selects Appeteasers', () => {
     //TODO: Add a test to check Appeteasers
+    cy.contains('Appeteasers').click();
+    //Check if Tater Tots is there.
+    cy.contains('Tater Tots');
+    //Check if Buffalo Wings is there.
+    cy.contains('Buffalo Wings');
+    //Make sure that the Handhelds DO NOT EXIST
+    cy.contains('Cheese Burger').should('not.exist');
+    cy.contains('Fajita Tacos').should('not.exist');
   });
 });
